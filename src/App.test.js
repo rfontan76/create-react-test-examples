4209ed5398a9cfb8f67237291bfaaf23445e7551
Rodrigo Fontan <rodrigo.fontan@globant.com>
2019-03-19 15:01:28 -0300

    Updating Test folder

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme'
 import ReactDOM from 'react-dom';
-import App from '../App';
-import { total, getUserName, getUserFullName } from '../App';
+import App from './App';
+import { total, getUserName, getUserFullName } from './App';
 import logo from '../logo.svg';
 
 
